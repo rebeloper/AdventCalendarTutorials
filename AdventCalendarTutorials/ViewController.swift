aa119450cb5a88188325f6e3e593109a94e81807
Alex <therebeloper@gmail.com>
2017-12-01 14:22:43 +0200

    Initial Commit

---
+++ AdventCalendarTutorials/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdventCalendarTutorials
+//
+//  Created by Alex Nagy on 01/12/2017.
+//  Copyright © 2017 Alex Nagy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
