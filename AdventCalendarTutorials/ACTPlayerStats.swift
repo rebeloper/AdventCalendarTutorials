87bc3be6ce92dc90c0364192977163dcb604d4a7
Alex <therebeloper@gmail.com>
2017-12-12 09:44:37 +0200

    day 11

--- AdventCalendarTutorials/ACTPlayerStats.swift
+++ AdventCalendarTutorials/ACTPlayerStats.swift
@@ -13,6 +13,8 @@ let kBackgroundMusicName = "BackgroundMusic"
 let kBackgroundMusicExtension = "mp3"
 let kSoundState = "kSoundState"
 let kNoAdsState = "kNoAdsState"
+let kScore = "kScore"
+let kBestScore = "kBestScore"
 
 enum SoundFileName: String {
   case TapFile = "Tap.wav"
@@ -22,6 +24,29 @@ class ACTPlayerStats {
   
   private init() {}
   static let shared = ACTPlayerStats()
+  
+  func setScore(_ value: Int) {
+    
+    if value > getBestScore() {
+      setBestScore(value)
+    }
+    
+    UserDefaults.standard.set(value, forKey: kScore)
+    UserDefaults.standard.synchronize()
+  }
+  
+  func getScore() -> Int {
+    return UserDefaults.standard.integer(forKey: kScore)
+  }
+  
+  func setBestScore(_ value: Int) {
+    UserDefaults.standard.set(value, forKey: kBestScore)
+    UserDefaults.standard.synchronize()
+  }
+  
+  func getBestScore() -> Int {
+    return UserDefaults.standard.integer(forKey: kBestScore)
+  }
   
   func setSounds(_ state: Bool) {
     UserDefaults.standard.set(state, forKey: kSoundState)
