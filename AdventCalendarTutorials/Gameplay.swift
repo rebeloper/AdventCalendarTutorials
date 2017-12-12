a188cb577590af6e5da86bc94f926151f0243d58
Alex <therebeloper@gmail.com>
2017-12-12 10:05:52 +0200

    day 12

--- AdventCalendarTutorials/Gameplay.swift
+++ AdventCalendarTutorials/Gameplay.swift
@@ -22,12 +22,32 @@ class Gameplay: SKScene {
     return button
   }()
   
+  lazy var countdownLabel: SKLabelNode = {
+    var label = SKLabelNode(fontNamed: "BubbleGum")
+    label.fontSize = CGFloat.universalFont(size: 36)
+    label.zPosition = 2
+    label.color = SKColor.white
+    label.horizontalAlignmentMode = .left
+    label.verticalAlignmentMode = .center
+    label.text = "\(counterStartValue)"
+    return label
+  }()
+  
+  var counter = 0
+  var counterTimer = Timer()
+  var counterStartValue = 60
   
   override func didMove(to view: SKView) {
     print("Inside Gameplay")
     backgroundColor = .cyan
     backButton.position = CGPoint(x: ScreenSize.width * 0.2, y: ScreenSize.heigth * 0.85)
     addChild(backButton)
+    
+    countdownLabel.position = CGPoint(x: ScreenSize.width * 0.2, y: ScreenSize.heigth * 0.79)
+    addChild(countdownLabel)
+    
+    counter = counterStartValue
+    startCounter()
   }
   
   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -37,6 +57,15 @@ class Gameplay: SKScene {
 //
 //      }
 //    }
+  }
+  
+  func startCounter() {
+    counterTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(decrementCounter), userInfo: nil, repeats: true)
+  }
+  
+  @objc func decrementCounter() {
+    counter -= 1
+    countdownLabel.text = "\(counter)"
   }
 }
 
